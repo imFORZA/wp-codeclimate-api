dc11087b504bc4240eeca5d68ff5acce7fe6921c
Brandon Hubbard <bkhubbard@gmail.com>
2016-09-02 20:03:29 -0700

    docs

--- wp-codeclimate-api.php
+++ wp-codeclimate-api.php
@@ -9,29 +9,61 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 
+/**
+ * CodeClimateAPI class.
+ */
 class CodeClimateAPI {
-	
+
 	/**
 	 * [__construct description]
 	 * @param [type] $api_key [description]
 	 */
 	public function __construct( $api_key = null ) {
-		
 	}
-	
+
+	/**
+	 * get_repos function.
+	 *
+	 * @access protected
+	 * @return void
+	 */
 	protected function get_repos(){
 	}
-	
+
+	/**
+	 * get_repo function.
+	 *
+	 * @access protected
+	 * @return void
+	 */
 	protected function get_repo(){
 	}
-	
+
+	/**
+	 * get_repo_branches function.
+	 *
+	 * @access protected
+	 * @return void
+	 */
 	protected function get_repo_branches(){
 	}
 
+	/**
+	 * refresh_repo function.
+	 *
+	 * @access protected
+	 * @return void
+	 */
 	protected function refresh_repo(){
 	}
-	
+
+	/**
+	 * refresh_repo_branches function.
+	 *
+	 * @access protected
+	 * @return void
+	 */
 	protected function refresh_repo_branches(){
 	}
-	
+
 }
