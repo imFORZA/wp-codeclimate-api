2f2b027ac6f2b8b6812c83161ad8b98872adf10c
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-09-02 20:01:29 -0700

    Update wp-codeclimate-api.php

--- wp-codeclimate-api.php
+++ wp-codeclimate-api.php
@@ -9,8 +9,29 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 
+class CodeClimateAPI {
+	
+		/**
+	 * [__construct description]
+	 * @param [type] $api_key [description]
+	 */
+	public function __construct( $api_key = null ) {
+		
+	}
+	
+	protected function get_repos(){
+	}
+	
+	protected function get_repo(){
+	}
+	
+	protected function get_repo_branches(){
+	}
 
-
-
-
-
+	protected function refresh_repo(){
+	}
+	
+	protected function refresh_repo_branches(){
+	}
+	
+}
