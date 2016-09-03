f2c31c3bcbac5a3caf537cda7d8d5c9b7fc3bba7
Brandon Hubbard <bkhubbard@gmail.com>
2016-09-02 18:07:20 -0700

    codeclimate api

---
+++ wp-codeclimate-api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* WP CodeClimate API (https://codeclimate.com/docs/api)
+*
+* @package WP-CodeClimate-API
+*/
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+
+
+
+
+
+
