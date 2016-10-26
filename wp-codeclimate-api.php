5b363a8c1d4b55c170fa25db9407e8f854587b92
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-10-26 13:27:16 -0700

    Update wp-codeclimate-api.php

--- wp-codeclimate-api.php
+++ wp-codeclimate-api.php
@@ -5,6 +5,17 @@
  * @package WP-CodeClimate-API
  */
 
+/*
+* Plugin Name: WP CodeClimate API
+* Plugin URI: https://github.com/wp-api-libraries/wp-codeclimate-api
+* Description: Perform API requests to CodeClimate in WordPress.
+* Author: WP API Libraries
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-codeclimate-api
+* GitHub Branch: master
+*/
+
 /* Exit if accessed directly */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
